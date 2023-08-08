dfc0f022fa8b0d09e399cee43289492992b124e8
davidkirkby <drkirkby@kirkbymicrowave.co.uk>
2023-08-08 15:48:34 +0100

    Have sepparate files set_initial_voltages_on_3D_double_array.c amd set_initial_voltages_on_3D_float_array.c to make comparisions with diff easier

---
+++ src/set_initial_voltages_on_3D_float_array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cppcc.h"
+
+void set_initial_voltages_on_3D_float_array(float *** voltage_array, unsigned int r1, unsigned int t1, unsigned int r2, unsigned int t2, unsigned int x, unsigned int length, unsigned int width_and_height) { 
+}
+
+
