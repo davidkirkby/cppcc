e7ae50779dbd8d829884c83f8cee765c542d9da2
davidkirkby <drkirkby@kirkbymicrowave.co.uk>
2023-08-08 15:52:00 +0100

    remove set_initial_voltages.c as it's been replaced by other files

--- src/set_initial_voltages.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "cppcc.h"
-
-void set_initial_float_voltages(float *** voltage array, unsigned int r1, unsigned int t1, unsigned int r2, unsigned int t2, unsigned int x, unsigned int length, unsigned int width_and_height);
-
-void set_initial_double_voltages(double *** voltage array, unsigned int r1, unsigned int t1, unsigned int r2, unsigned int t2, unsigned int x, unsigned int length, unsigned int width_and_height);
-
